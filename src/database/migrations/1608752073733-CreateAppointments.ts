ac1414d9099fffacd77828c6903489223d53bfc4
Neto <jjns.januario@gmail.com>
2020-12-24 11:21:19 -0300

    database conection

--- src/database/migrations/1608752073733-CreateAppointments.ts
+++ src/database/migrations/1608752073733-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1608752073733
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
